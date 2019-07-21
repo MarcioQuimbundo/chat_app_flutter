d8c10cf3bf76026e4b1bfd9b7d3a66e12917b4be
Marcio Teixeira Quimbundo [DIGITAL FACTORY] <marcioquimbundo@hotmail.com>
2019-07-21 23:45:29 +0100

    ios and default theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-void main() {
-  Firestore.instance.collection("teste").document("teste").setData({"teste1":"teste2"});
-  runApp(MaterialApp(home: HomePage(),));
+
+void main() async {
+  runApp(MyApp());
 }
 
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
 
-  _HomePageState createState() => _HomePageState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      title: "Chat Concierge",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  ChatScreen({Key key}) : super(key: key);
+
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Concierge Chat"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[],
+        ),
+      ),
     );
   }
 }
