656530c3900fb2387cac8688a0ca8f6435e0d6ce
Marcio Teixeira Quimbundo [DIGITAL FACTORY] <marcioquimbundo@hotmail.com>
2019-07-21 22:10:56 +0100

    gradles config and companies finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+void main() {
+  Firestore.instance.collection("teste").document("teste").setData({"teste1":"teste2"});
+  runApp(MaterialApp(home: HomePage(),));
+}
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+    return Container(
     );
   }
 }
