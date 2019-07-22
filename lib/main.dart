9c1166f79268e1738a1ec4da15a9c385c5070d14
Marcio Teixeira Quimbundo [DIGITAL FACTORY] <marcioquimbundo@hotmail.com>
2019-07-22 13:26:53 +0100

    ChatMessage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -11,7 +12,7 @@ final ThemeData kIOSTheme = ThemeData(
     primaryColorBrightness: Brightness.light);
 
 final ThemeData kDefaultTheme = ThemeData(
-    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
+    primarySwatch: Colors.amber, accentColor: Colors.orangeAccent[400]);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
@@ -49,8 +50,120 @@ class _ChatScreenState extends State<ChatScreen> {
               Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  TextComposer({Key key}) : super(key: key);
+
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isComposing ? () {} : null)
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isComposing ? () {} : null,
+                    ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://avatars3.githubusercontent.com/u/29952508?s=460&v=4"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Márcio",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("teste"),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
